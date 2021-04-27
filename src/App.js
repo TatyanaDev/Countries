961bdc015f688216c1aac918aa02e5b1ead5f3dd
Tatyana404 <kot.kate2020@gmail.com>
2021-04-27 15:59:47 +0300

    worker search

--- src/App.js
+++ src/App.js
@@ -95,22 +95,34 @@ function App () {
             </tr>
           </thead>
           <tbody>
-            {data.map((country, index) => {
-              return (
-                <>
-                  <tr key={index}>
-                    <td className={styles.colName}>{country.name}</td>
-                    <td className={styles.colCapital}>{country.capital}</td>
-                    <td className={styles.colPopulation}>
-                      {country.population}
-                    </td>
-                    <td className={styles.colCallingCodes}>
-                      {country.callingCodes}
-                    </td>
-                  </tr>
-                </>
-              );
-            })}
+            {data
+              .filter(country => {
+                if (searchTerm === '') {
+                  return country;
+                } else if (
+                  country.name
+                    .toLowerCase()
+                    .indexOf(searchTerm.toLowerCase()) === 0
+                ) {
+                  return country;
+                }
+              })
+              .map((country, index) => {
+                return (
+                  <>
+                    <tr key={index}>
+                      <td className={styles.colName}>{country.name}</td>
+                      <td className={styles.colCapital}>{country.capital}</td>
+                      <td className={styles.colPopulation}>
+                        {country.population}
+                      </td>
+                      <td className={styles.colCallingCodes}>
+                        {country.callingCodes}
+                      </td>
+                    </tr>
+                  </>
+                );
+              })}
           </tbody>
         </table>
       )}
