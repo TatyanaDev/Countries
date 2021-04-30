3eee7339b338c5f8c5659c7400a46bbf1011baa1
Tatyana404 <kot.kate2020@gmail.com>
2021-04-30 13:12:41 +0300

    Add tbody module

--- src/components/TableCountry/Table/index.jsx
+++ src/components/TableCountry/Table/index.jsx
@@ -1,36 +1,12 @@
 import styles from './table.module.scss';
 import Thead from './Thead/index';
+import Tbody from './Tbody/index';
 
 const Table = ({ data, flag, setFlag, searchTerm }) => {
   return (
     <table className={styles.container}>
-      <Thead data={data} flag={flag} setFlag={setFlag}/>
-      <tbody>
-        {data
-          .filter(country => {
-            if (searchTerm === '') {
-              return country;
-            } else if (
-              country.name.toLowerCase().indexOf(searchTerm.toLowerCase()) === 0
-            ) {
-              return country;
-            }
-          })
-          .map((country, index) => {
-            return (
-              <>
-                <tr key={index}>
-                  <td className={styles.colName}>{country.name}</td>
-                  <td className={styles.colCapital}>{country.capital}</td>
-                  <td className={styles.colPopulation}>{country.population}</td>
-                  <td className={styles.colCallingCodes}>
-                    {country.callingCodes}
-                  </td>
-                </tr>
-              </>
-            );
-          })}
-      </tbody>
+      <Thead data={data} flag={flag} setFlag={setFlag} />
+      <Tbody data={data} searchTerm={searchTerm} />
     </table>
   );
 };
